1d2429a5d0c178000ecf010d5671e7a5425ee7f3
hulaizh <hulaizh@gmail.com>
2021-11-08 12:41:50 +0100

    cleandata update

--- src/cleandata.py
+++ src/cleandata.py
@@ -1,4 +1,4 @@
 import pandas as pd
 
-dta_manager = pd.read_stata('data/Koijen Yogo Data/Manager.dta')
-dta_manager.describe()
+# dta_manager = pd.read_stata('data/Koijen Yogo Data/Manager.dta')
+# dta_manager.describe()
